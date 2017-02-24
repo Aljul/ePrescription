d05b7538527a9cbcd76185ef277fde5262c2da79
Alexandre Julien <alexandre.julien@live.ca>
2017-02-24 18:06:39 +0000

    basic routes and views from doctors and users (no css)

---
+++ routes/doctors.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/login", (req, res) => {
+    res.render("doctors_login");
+  });
+
+  return router;
+}
