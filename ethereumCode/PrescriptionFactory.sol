c0e572d4f4e58450fa3fc438501b794a38977eb6
Dev43 <patrick.guay43@gmail.com>
2017-02-23 23:30:25 -0500

    add second prescription contract template and tests

--- ethereumCode/PrescriptionFactory.sol
+++ ethereumCode/PrescriptionFactory.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.4;
 
+import "./Prescription.sol";
+
 contract PrescriptionFactory {
 
   address public owner;
   mapping (address => bool) doctors; // stores public keys of doctors and pharmacies that are trusted
   mapping (address => bool) pharmacies; // can only be added by the owner
-
+  address[] prescriptionAddresses;
   function PrescriptionFactory() {
     // constructor
     owner = msg.sender;
@@ -40,7 +42,21 @@ contract PrescriptionFactory {
     selfdestruct(this);
   }
 
+  function createPrescription() returns(Prescription inside){
+    address newPrescription = new Prescription();
+    prescriptionAddresses.push(newPrescription);
+    // Prescription pres = Prescription(newPrescription);
+    // // bytes32 toReturn  = newPrescription.Astring;
+    // return pres;
 
+  }
+// need to do it in 2 steps, when you make a transaction, it returns the txn receipt and stuff
+// so a new accessor function will get what you need.
+// make sure to write constant in front of it or it becomes a transaction
+  function getInfo(uint i) constant returns(bytes32 ok){
+    Prescription p = Prescription(prescriptionAddresses[i]);
+    return p.Astring();
+  }
 
 
 }
@@ -56,3 +72,5 @@ contract PrescriptionFactory {
  // PrescriptionFactory.deployed().then(r => {console.log(r.owner.call().then(console.log))})
  // PrescriptionFactory.deployed().then(r => {console.log(r.destroy())})
  // PrescriptionFactory.deployed().then(r => {console.log(r.owner.call().then(console.log))})
+
+//  PrescriptionFactory.deployed().then(function(r) {return r.isDoctorTrusted(0x938fdc87b4b1fa4b83e301abe978569d9c85d636)})
