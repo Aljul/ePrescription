a04dd27ee4af6b63860fc2261538b9779a669cbf
Dev43 <patrick.guay43@gmail.com>
2017-02-27 13:08:21 -0500

    add connection to testrpc for accounts management

--- routes/lib/ethereum-contracts.js
+++ routes/lib/ethereum-contracts.js
@@ -3,9 +3,10 @@ const contract                        = require("truffle-contract");
 const PrescriptionFactoryJSON         = require('../../ethereumCode/build/contracts/PrescriptionFactory.json');
 const PrescriptionJSON                = require('../../ethereumCode/build/contracts/Prescription.json')
 const AbstractPrescriptionFactoryJSON = require('../../ethereumCode/build/contracts/AbstractPrescriptionFactory.json')
-const web3 = new Web3();
 var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-
+const web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+// connect web3 to the testrpc, so you get all the test accounts with valid public/private keys
 
 const PrescriptionFactory         = contract(PrescriptionFactoryJSON);
 const Prescription                = contract(PrescriptionJSON);
@@ -15,17 +16,21 @@ PrescriptionFactory.setProvider(provider);
 Prescription.setProvider(provider);
 AbstractPrescriptionFactory.setProvider(provider);
 
+console.log(web3.eth.accounts)
 
-// two random accounts taken from testrpc (9) and (8)
-var account_one = "0x0c6fd4d7caf93f82baa678c70ae51dc45f099b6c";
-var account_two = "0x76630416aa298a647c558910def6833f711dbd3c";
 
 // this would be the actual address where the contract is deployed
-const prescriptionFactory_addr = "0xd6a2628d0cdfc8bf3e1898e87c6f423bb595a6f4";
+const prescriptionFactory_addr = "0x22b3557ebf8061df8e0f96fd31d74017371b6249";
 
 
 PrescriptionFactory.at(prescriptionFactory_addr)
-.then((instance) => {console.log(instance)})
+.then((instance) => {
+  factory = instance;
+  // console.log(factory)
+  return factory.owner()
+}).then((ownerAddress) => {
+  console.log(ownerAddress);
+})
 .catch((err) => {console.log(err)})
 
 
