8da753ee86e3147cd15b5498e5775e080134c9e1
Alexandre Julien <alexandre.julien@live.ca>
2017-02-28 00:00:26 +0000

    logout implemented, show full name of person logged in in navbar

--- routes/main.js
+++ routes/main.js
@@ -9,17 +9,28 @@ module.exports = (knex) => {
   // ***** GET routes *****
 
   router.get("/", (req, res) => {
-    // if session id (check if doctor or patient)
-    res.render("main");
-    // else redirect login
+    if (req.session["user_id"]) {
+      //(check if doctor or patient)
+      knex
+        .select("first_name", "last_name")
+        .from("users")
+        .where("id", req.session["user_id"])
+        .then((result) => {
+          let username = `${result[0].first_name} ${result[0].last_name}`;
+          res.render("main", { username: username });
+        });
+    } else { res.redirect("login"); }
   });
 
   router.get("/login", (req, res) => {
-    res.render("login");
+    if (!req.session["user_id"]) {
+      res.render("login");
+    } else { res.redirect("/"); }
   });
 
   router.get("/logout", (req, res) => {
-    res.send("logout");
+    req.session["user_id"] = null;
+    res.redirect('/login');
   });
 
   router.get("/register", (req, res) => {
@@ -29,6 +40,7 @@ module.exports = (knex) => {
   //  ***** POST routes *****
 
   router.post("/login", (req, res) => {
+    // add escape function later
     let emailField = req.body.email;
     let pwField = req.body.password;
     knex
@@ -39,7 +51,7 @@ module.exports = (knex) => {
         if (!result[0]) {
           res.send("invalid email");
         } else if (result[0] && bcrypt.compareSync(pwField, result[0].password_digest)) {
-          req.session["user_id"] = result[0].user_id;
+          req.session["user_id"] = result[0].id;
           res.redirect("/");
         } else {
           res.status(401).send("invalid password!");
