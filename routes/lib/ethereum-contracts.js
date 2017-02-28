ebc90930b9ddd0ace7a69decbde2039df636e448
Dev43 <patrick.guay43@gmail.com>
2017-02-28 15:26:58 -0500

    moved ethereum web3 tests to its own file

--- routes/lib/ethereum-contracts.js
+++ routes/lib/ethereum-contracts.js
@@ -13,152 +13,123 @@ web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 const PrescriptionFactory         = contract(PrescriptionFactoryJSON);
 const Prescription                = contract(PrescriptionJSON);
 const AbstractPrescriptionFactory = contract(AbstractPrescriptionFactoryJSON);
-
+const GAS = 4000000
 // set their providers (right now testrpc)
 PrescriptionFactory.setProvider(provider);
 Prescription.setProvider(provider);
 AbstractPrescriptionFactory.setProvider(provider);
 
-// seed if testrpc is clean
-var accountTotal = web3.eth.getBalance(web3.eth.accounts[0]).toNumber();
 
-// console.log(web3.eth.getBalance(web3.eth.accounts[0]).toNumber())
+module.exports = {
 
-if (accountTotal == 99860126700000000000){
-  seed.populatePrescriptionFactory(web3, PrescriptionFactory, Prescription);
-  seed.createPrescriptions(web3, PrescriptionFactory, Prescription);
-}
+  seedPrescriptionFactory: function(){
+    return seed.populatePrescriptionFactory(web3, PrescriptionFactory, Prescription);
+  },
 
+  seedPrescriptions: function(){
+   return seed.createPrescriptions(web3, PrescriptionFactory, Prescription);
+  },
 
-
-function publishPrescription(patientAddress, doctorAddress, prescriptionData, prescriptionName){
- return PrescriptionFactory.deployed().then(function(instance){
-  var contractInstance = instance;
-  return contractInstance.createPrescription(prescriptionName, prescriptionData, patientAddress, {from: doctorAddress, gas: 400000})
- }).then((message) => {
-  // console.log(message)
-  if(message.logs.length == 0){
-    throw 'Something went wrong when creating the prescription';
-  }
-  // console.log(message.logs[0].args._theAddress);
-  return message.logs[0].args._theAddress
- }).catch((err) => {
-  console.log(err)
-  return err;
-  })
-}
-
-// publishPrescription(web3.eth.accounts[2], web3.eth.accounts[1], "this is the prescripiton", 'name').then(console.log)
-
-function retrieveAllPrescriptionAddresses(patientAddress, doctorAddress){
-  return PrescriptionFactory.deployed().then(function(instance){
-  var contractInstance = instance;
-  return contractInstance.getAllPrescriptionsForPatient(patientAddress, {from: doctorAddress})
- }).then((message) => {
-  // console.log(message)
-  return message
- }).catch((err) => {
-  console.log(err)
-  return err;
-  })
-}
-// retrieveAllPrescriptionAddresses(web3.eth.accounts[3], web3.eth.accounts[0])
-
-function retrieveLatestPrescriptionAddress(patientAddress, doctorAddress){
-  return PrescriptionFactory.deployed().then(function(instance){
-  var contractInstance = instance;
-  return contractInstance.getLatestPrescriptionForPatient(patientAddress, {from: doctorAddress})
- }).then((message) => {
-  console.log(message)
-  return message
- }).catch((err) => {
-  console.log(err)
-  return err;
-  })
-}
-
-// retrieveLatestPrescriptionAddress(web3.eth.accounts[3], web3.eth.accounts[0])
-
-function getPrescriptionData(prescriptionAddress){
-  return Prescription.at(prescriptionAddress).then(function(instance){
-    prescription = instance;
-    console.log(instance);
-    return prescription.getPrescriptionData()
-  }).then((data) => {
-    console.log(data);
-    return web3.toAscii(data)
-  }).catch((err) => {
+  publishPrescription: function(patientAddress, doctorAddress, prescriptionData, prescriptionName){
+   return PrescriptionFactory.deployed().then(function(instance){
+    var contractInstance = instance;
+    return contractInstance.createPrescription(prescriptionName, prescriptionData, patientAddress, {from: doctorAddress, gas: GAS})
+   }).then((message) => {
+    // console.log(message)
+    if(message.logs.length == 0){
+      throw 'Something went wrong when creating the prescription';
+    }
+    // console.log(message.logs[0].args._theAddress);
+    return message.logs[0].args._theAddress
+   }).catch((err) => {
     console.log(err)
-  return err;
-  })
-}
-// retrieveLatestPrescriptionAddress(web3.eth.accounts[3], web3.eth.accounts[0])
-// .then((address) => {
-//   return getPrescriptionData(address)
-// }).then((data) => {
-
-//   console.log(data);
-// })
-
-
-function printPrescription(prescriptionAddress){
-  let prescriptionData, prescriptionName, docAddress, patientAddr;
-
- return Prescription.at(prescriptionAddress).then(function(instance){
-    prescription = instance;
-    // console.log(instance);
-    return prescription.getPrescriptionData()
-  }).then((data) => {
-    prescriptionData = web3.toAscii(data);
-    return prescription.name()
-  }).then((name) => {
-    prescriptionName = web3.toAscii(name);
-    return prescription.issuingDoctor()
-  }).then((doctor) => {
-    docAddress = doctor;
-    return prescription.getPatientAddress()
-  }).then((address) => {
-    patientAddr = address
-    var verbosePrescription = `Prescription
-    NAME: ${prescriptionName}
-    PATIENT ADDRESS: ${patientAddr}
-    ISSUED BY: ${docAddress}
-    DATA: ${prescriptionData}`;
-    return verbosePrescription
-  }).catch((err) => {
-    console.log("The error is", err)
     return err;
-  })
-}
+    })
+   },
 
-// retrieveLatestPrescriptionAddress(web3.eth.accounts[2], web3.eth.accounts[0])
-// .then((address) => {
-//   // console.log(address)
-//   return printPrescription(address)
-// }).then((data) => {
-//   console.log(data);
-// })
+  retrieveAllPrescriptionAddresses: function(patientAddress, doctorAddress){
+    return PrescriptionFactory.deployed().then(function(instance){
+    var contractInstance = instance;
+    return contractInstance.getAllPrescriptionsForPatient(patientAddress, {from: doctorAddress})
+   }).then((message) => {
+    // console.log(message)
+    return message
+   }).catch((err) => {
+    console.log(err)
+    return err;
+    })
+  },
+
+  retrieveLatestPrescriptionAddress: function(patientAddress, doctorAddress){
+    return PrescriptionFactory.deployed().then(function(instance){
+    var contractInstance = instance;
+    return contractInstance.getLatestPrescriptionForPatient(patientAddress, {from: doctorAddress})
+   }).then((message) => {
+    console.log(message)
+    return message
+   }).catch((err) => {
+    console.log(err)
+    return err;
+    })
+  },
 
 
-function fulfillPrescription(prescriptionAddress){
+  getPrescriptionData: function(prescriptionAddress){
+    return Prescription.at(prescriptionAddress).then(function(instance){
+      prescription = instance;
+      console.log(instance);
+      return prescription.getPrescriptionData()
+    }).then((data) => {
+      console.log(data);
+      return web3.toAscii(data)
+    }).catch((err) => {
+      console.log(err)
+    return err;
+    })
+  },
+
+  printPrescription: function(prescriptionAddress){
+    let prescriptionData, prescriptionName, docAddress, patientAddr;
+
    return Prescription.at(prescriptionAddress).then(function(instance){
-    prescription = instance;
-    // console.log(instance);
-    return prescription.destroy({from: web3.eth.accounts[0]})
-  }).then((response) => {
-    // console.log(response)
-    return response
-  }).catch((err) => {
-    console.log("The error is", err)
-    return err;
-  })
+      prescription = instance;
+      // console.log(instance);
+      return prescription.getPrescriptionData()
+    }).then((data) => {
+      prescriptionData = web3.toAscii(data);
+      return prescription.name()
+    }).then((name) => {
+      prescriptionName = web3.toAscii(name);
+      return prescription.issuingDoctor()
+    }).then((doctor) => {
+      docAddress = doctor;
+      return prescription.getPatientAddress()
+    }).then((address) => {
+      patientAddr = address
+      var verbosePrescription = `Prescription
+      NAME: ${prescriptionName}
+      PATIENT ADDRESS: ${patientAddr}
+      ISSUED BY: ${docAddress}
+      DATA: ${prescriptionData}`;
+      return verbosePrescription
+    }).catch((err) => {
+      console.log("The error is", err)
+      return err;
+    })
+  },
+
+  fulfillPrescription: function(prescriptionAddress){
+     return Prescription.at(prescriptionAddress).then(function(instance){
+      prescription = instance;
+      // console.log(instance);
+      return prescription.destroy({from: web3.eth.accounts[0]})
+    }).then((response) => {
+      // console.log(response)
+      return response
+    }).catch((err) => {
+      console.log("The error is", err)
+      return err;
+    })
+  }
+
 }
-
-
-// retrieveLatestPrescriptionAddress(web3.eth.accounts[2], web3.eth.accounts[0])
-// .then((address) => {
-//   // console.log(address)
-//   return fulfillPrescription(address)
-// }).then((response) => {
-//   console.log(response);
-// })
--- routes/lib/test-eth-contracts.js
+++ routes/lib/test-eth-contracts.js
@@ -1,26 +1,58 @@
 const Web3                            = require('web3');
 const contract                        = require("truffle-contract");
-const PrescriptionFactoryJSON         = require('../../ethereumCode/build/contracts/PrescriptionFactory.json');
-const PrescriptionJSON                = require('../../ethereumCode/build/contracts/Prescription.json')
-const AbstractPrescriptionFactoryJSON = require('../../ethereumCode/build/contracts/AbstractPrescriptionFactory.json')
-const seed                            = require('./eth-seed.js');
 var provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const web3   = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 // connect web3 to the testrpc, so you get all the test accounts with valid public/private keys
 
-// set the contract abstractions so we can directly call their functions
-const PrescriptionFactory         = contract(PrescriptionFactoryJSON);
-const Prescription                = contract(PrescriptionJSON);
-const AbstractPrescriptionFactory = contract(AbstractPrescriptionFactoryJSON);
-
-// set their providers (right now testrpc)
-PrescriptionFactory.setProvider(provider);
-Prescription.setProvider(provider);
-AbstractPrescriptionFactory.setProvider(provider);
-
-
 const rpcCalls= require('./ethereum-contracts.js');
 
+// seed if testrpc is clean
+var accountTotal = web3.eth.getBalance(web3.eth.accounts[0]).toNumber();
 
-// rpcCalls.publishPrescription(web3.eth.accounts[2], web3.eth.accounts[1], "this is the prescripiton", 'name').then(console.log)
+
+  rpcCalls.seedPrescriptionFactory().then(() => {
+
+  return rpcCalls.seedPrescriptions();
+  }).then( () => {
+
+
+
+rpcCalls.publishPrescription(web3.eth.accounts[2], web3.eth.accounts[1], "this is the prescripiton", 'name').then(console.log)
+
+rpcCalls.publishPrescription(web3.eth.accounts[2], web3.eth.accounts[1], "this is the prescripiton", 'name').then(console.log)
+
+rpcCalls.retrieveAllPrescriptionAddresses(web3.eth.accounts[3], web3.eth.accounts[0])
+
+rpcCalls.retrieveLatestPrescriptionAddress(web3.eth.accounts[3], web3.eth.accounts[0])
+
+
+rpcCalls.retrieveLatestPrescriptionAddress(web3.eth.accounts[3], web3.eth.accounts[0])
+.then((address) => {
+  return rpcCalls.getPrescriptionData(address)
+}).then((data) => {
+
+  console.log(data);
+})
+
+rpcCalls.retrieveLatestPrescriptionAddress(web3.eth.accounts[2], web3.eth.accounts[0])
+.then((address) => {
+  // console.log(address)
+  return rpcCalls.printPrescription(address)
+}).then((data) => {
+  console.log(data);
+})
+
+
+rpcCalls.retrieveLatestPrescriptionAddress(web3.eth.accounts[2], web3.eth.accounts[0])
+.then((address) => {
+  theAddress = address;
+  // console.log(address)
+  return rpcCalls.printPrescription(theAddress)
+}).then(() => {
+  return rpcCalls.fulfillPrescription(theAddress)
+}).then((response) => {
+  console.log(response);
+})
+
+})
