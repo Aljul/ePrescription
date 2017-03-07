cd60c9615400bc295a93e98d68d8a23000d02abd
Dev43 <patrick.guay43@gmail.com>
2017-03-07 17:47:37 -0500

    changed settings to connect to azure blockchain

--- routes/lib/eth-seeder-driver.js
+++ routes/lib/eth-seeder-driver.js
@@ -5,14 +5,14 @@ const seed                            = require('./eth-seed.js');
 const PrescriptionFactoryJSON         = require('../../ethereumCode/build/contracts/PrescriptionFactory.json');
 const PrescriptionJSON                = require('../../ethereumCode/build/contracts/Prescription.json')
 const AbstractPrescriptionFactoryJSON = require('../../ethereumCode/build/contracts/AbstractPrescriptionFactory.json')
-
+const rpcCalls                        = require('./ethereum-contracts.js');
 const web3   = new Web3();
 // var provider = new Web3.providers.HttpProvider("http://localhost:8545");
 // web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-// web3.setProvider(new web3.providers.HttpProvider('http://rxlhlvoxq.eastus.cloudapp.azure.com:8545'));
-// var provider = new Web3.providers.HttpProvider('http://rxlhlvoxq.eastus.cloudapp.azure.com:8545');
-var provider = new Web3.providers.HttpProvider("http://localhost:4000");
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:4000'));
+web3.setProvider(new web3.providers.HttpProvider('http://rxlhlvoxq.eastus.cloudapp.azure.com:8545'));
+var provider = new Web3.providers.HttpProvider('http://rxlhlvoxq.eastus.cloudapp.azure.com:8545');
+// var provider = new Web3.providers.HttpProvider("http://localhost:4000");
+// web3.setProvider(new web3.providers.HttpProvider('http://localhost:4000'));
 
 // set the contract abstractions so we can directly call their functions
 const PrescriptionFactory         = contract(PrescriptionFactoryJSON);
@@ -30,6 +30,7 @@ AbstractPrescriptionFactory.setProvider(provider);
 // web3.personal.unlockAccount(web3.eth.accounts[2], process.env.ETH_ACCOUNT_PASSWORD)
 // web3.personal.unlockAccount(web3.eth.accounts[3], process.env.ETH_ACCOUNT_PASSWORD)
 // // connect web3 to the testrpc, so you get all the test accounts with valid public/private keys
+let prescriptionAddress;
 seed.deployContracts(web3, PrescriptionFactory, Prescription)
 .then(() => {
   seed.initializeAddresses(web3)
@@ -37,6 +38,10 @@ seed.deployContracts(web3, PrescriptionFactory, Prescription)
 .then(() => {
   return seed.populatePrescriptionFactory(web3, PrescriptionFactory, Prescription)
 })
-.then(() => {
+.then((address) => {
+  prescriptionAddress = address
   return seed.createPrescriptions(web3, PrescriptionFactory, Prescription);
 })
+.then(() => {
+  return rpcCalls.printPrescription(prescriptionAddress)
+}).then(console.log)
